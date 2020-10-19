e0a40302793a834329e16f8e0a850589f862008c
Yash Kumar Sharma <yks.nit@gmail.com>
2020-10-19 18:47:25 +0530

    Bare minimum changes required to run this demo with latest react-native-gifted-chat (0.16.0)

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,7 @@
 module.exports = function override(config, env) {
   config.module.rules.push({
     test: /\.js$/,
-    exclude: /node_modules[/\\](?!react-native-gifted-chat|react-native-lightbox|react-native-parsed-text|expo-av)/,
+    exclude: /node_modules[/\\](?!react-native-gifted-chat|react-native-lightbox|react-native-parsed-text|expo-av|react-native-typing-animation)/,
     use: {
       loader: 'babel-loader',
       options: {
